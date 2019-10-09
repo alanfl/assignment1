04dba31d44faed9d23b684fc9e42c1866c1fd80b
magnes10126@gmail.com <jdm362@pascal.cs.rutgers.edu>
2019-10-09 00:22:20 -0400

    Implement workloads B and C.

--- memgrind.c
+++ memgrind.c
@@ -14,7 +14,9 @@ int randomNum(int floor, int ceil){
 	return 0;
 }
 
-//return values of A-F will be the elapsed time in milliseconds
+//return values of A-F will be the elapsed time in milliseconds.
+//Remove /1000 from elapsed calculation if microseconds wanted
+
 //A: malloc() 1 byte and immediately free it - do this 150 times
 double malloc_and_free_150(){
 	//find start time
@@ -37,32 +39,124 @@ double malloc_and_free_150(){
 }
 
 
-/*
-B: malloc() 1 byte, store the pointer in an array - do this 150 times.
-Once you've malloc()ed 50 byte chunks, then free() the 50 1 byte pointers one by one.
-*/
-double malloc_store_free(){
+
+//B: malloc() 1 byte, store the pointer in an array - do this 150 times.
+//Once you've malloc()ed 50 byte chunks, then free() the 50 1 byte pointers
+//one by one.
+
+double malloc_store_free(){	
+	//find start time	
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+
+	char* ptrs [50];
+	int i,j;
+	//run 150 times
+	for(i = 0; i < 3; i++){
+		//malloc 50 bytes
+		for(j = 0; j < 50; j++){
+			char* test = malloc(1);
+			ptrs[j] = test;
+		}
+		//free 50 pointers
+		for(j = 0; j < 50; j++){
+			free(ptrs[j]);
+		}
+	}
 	
-	return 0.0;
+	//find end time, calculate length, return
+	gettimeofday(&end, NULL);
+	double elapsed = (end.tv_sec - start.tv_sec) + 
+              ((end.tv_usec - start.tv_usec)/1000.0);
+
+	return elapsed;
 
 }
 
-//C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
-double random_malloc_free_50(){
+//C: Randomly choose between a 1 byte malloc() or freeing a 1 byte pointer
+//malloc 50 times, if reach 50 mallocs free all, keep track of freed pointers
 
-	return 0.0;
+double random_malloc_free_50(){
+	//find start time	
+	struct timeval start, end;
+	gettimeofday(&start, NULL);	
+
+	//keep track of counts and type
+	int mCount = 0;
+	int fCount = 0;
+	int type;
+	char* ptrs [50];	
+
+	//run until the 50th malloc
+	while(mCount < 50){
+
+		//randomly pick between malloc and free, if cannot free then malloc
+		//increment counters after each malloc or free
+		type = rand() % 2; //even will be malloc, odd free
+
+		//expected free and there is something to free	
+		if(type == 1 && (mCount!=fCount)){
+			free(ptrs[fCount]);
+			fCount++;
+
+		//expected malloc or unable to free
+		}else{
+			char* test = malloc(1);
+			ptrs[mCount] = test;
+			mCount++;			
+		}
+		
+		//all 50 mallocs have occurred. free all remaining pointers
+		if(mCount == 50){
+			while(fCount < 50){
+				free(ptrs[fCount]);
+				fCount++;
+			}
+		}
+	}
+	
+	
+		
+	//find end time, calculate length, return
+	gettimeofday(&end, NULL);
+	double elapsed = (end.tv_sec - start.tv_sec) + 
+              ((end.tv_usec - start.tv_usec)/1000.0);
+
+	return elapsed;
 }
 
 //D: Randomly choose between a randomly-sized malloc() or free()ing a pointer 
 double all_random(){
+	//find start time	
+	struct timeval start, end;
+	gettimeofday(&start, NULL);	
 
-	return 0.0;
+	
+
+
+	//find end time, calculate length, return
+	gettimeofday(&end, NULL);
+	double elapsed = (end.tv_sec - start.tv_sec) + 
+              ((end.tv_usec - start.tv_usec)/1000.0);
+
+	return elapsed;
 }
 
 //E: Undetermined
 double filler_1(){
+//find start time	
+	struct timeval start, end;
+	gettimeofday(&start, NULL);	
 
-	return 0.0;
+	
+
+
+	//find end time, calculate length, return
+	gettimeofday(&end, NULL);
+	double elapsed = (end.tv_sec - start.tv_sec) + 
+              ((end.tv_usec - start.tv_usec)/1000.0);
+
+	return elapsed;
 }
 
 //F: Undetermined
@@ -97,7 +191,9 @@ int main (int argc, char* argv[]){
 		sums[5] += filler_2(); 
 	}
 
+
 	for(i = 0; i < 6; i++){
+		printf("Sum %d = %f\n", i, sums[i]);
 		averages[i] = sums[i] / 100.0;
 		printf("Mean time for workload %c = %f milliseconds\n", ('A'+ i), averages[i]);
 	}
