309478e7aab57da673b635cf2c5bb322c3b22cc0
jmagnes362 <jdm362@pascal.cs.rutgers.edu>
2019-10-08 22:03:08 -0400

    Implement workload A

--- memgrind.c
+++ memgrind.c
@@ -8,44 +8,67 @@
 #include <time.h>
 
 
-//A: malloc() 1 byte and immediately free it - do this 150 times
-int malloc_and_free_150(){
-	
+//random number generator
+int randomNum(int floor, int ceil){
+
 	return 0;
 }
+
+//return values of A-F will be the elapsed time in milliseconds
+//A: malloc() 1 byte and immediately free it - do this 150 times
+double malloc_and_free_150(){
+	//find start time
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	
+	//malloc and free 150 times	
+	int i = 0;
+	for(i = 0; i < 150; i++){
+		char* test = malloc(1);
+		free(test);
+	}
+
+	//find end time and return
+	gettimeofday(&end, NULL);
+	double elapsed = (end.tv_sec - start.tv_sec) + 
+              ((end.tv_usec - start.tv_usec)/1000.0);
+
+	return elapsed;
+}
+
 
 /*
 B: malloc() 1 byte, store the pointer in an array - do this 150 times.
 Once you've malloc()ed 50 byte chunks, then free() the 50 1 byte pointers one by one.
 */
-int malloc_store_free(){
+double malloc_store_free(){
 	
-	return 0;
+	return 0.0;
 
 }
 
 //C: Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer
-int random_malloc_free_50(){
+double random_malloc_free_50(){
 
-	return 0;
+	return 0.0;
 }
 
 //D: Randomly choose between a randomly-sized malloc() or free()ing a pointer 
-int all_random(){
+double all_random(){
 
-	return 0;
+	return 0.0;
 }
 
 //E: Undetermined
-int filler_1(){
+double filler_1(){
 
-	return 0;
+	return 0.0;
 }
 
 //F: Undetermined
-int filler_2(){
+double filler_2(){
 
-	return 0;
+	return 0.0;
 }
 
 
@@ -63,8 +86,8 @@ int main (int argc, char* argv[]){
 	//loop that runs all workloads back to back 100 times and calculates 
 	//mean runtime
 	int i = 0;
-	int sums [] = {0,0,0,0,0,0};
-	int averages [6];
+	double sums [] = {0.0,0.0,0.0,0.0,0.0,0.0};
+	double averages [6];
 	for(i = 0; i < 100; i++){
 		sums[0] += malloc_and_free_150();
 		sums[1] += malloc_store_free();
@@ -75,9 +98,10 @@ int main (int argc, char* argv[]){
 	}
 
 	for(i = 0; i < 6; i++){
-		averages[i] = sums[i] / 100;
-		printf("Mean time for workload %d = %d\n", i, averages[i]);
+		averages[i] = sums[i] / 100.0;
+		printf("Mean time for workload %c = %f milliseconds\n", ('A'+ i), averages[i]);
 	}
+
 
 	return 0;
 
