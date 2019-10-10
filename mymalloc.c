7d5df2bad383f0b4e4934c26355dfa2be77e9400
alanfl <alanfl81199@gmail.com>
2019-10-09 22:17:46 -0400

    Adjust header to use properly-scoped functions, add function definitions to source file.

--- mymalloc.c
+++ mymalloc.c
@@ -5,12 +5,41 @@
 #include "mymall.h"
 #include <stdio.h>
 
+
+// This is the header that precedes all managed blocks.
+// A negative value denotes that the block is not in use.
+// A positive value denotes that the block is in use.
+// All size values are in bytes.
+short size;
+
+// This constant refers to the size of the header, and should be referenced when calculating block jumps
+const size_t header_size = sizeof(size);
+
 // "Managed" block
 // Should be initialized as all 0s, since it's located in the BSS
 static char* heap[4096];
 
-void* mymalloc(size_t size, __FILE__, __LINE__) {
+// Usage: Traverses a linked list of block headers and returns a pointer to the first block that is meets the size
+// requirement and is also not in use.
+void* first_fit(size_t size);
 
-    // The block is not
+// Usage: Traverses a linked list of block headers and coalesces blocks that are both adjacent to each other, and freed.
+// Note: Coalescing will maintain lower addresses, and adjacent higher addresses will be absorbed
+void coalesce();
 
-}
+// Splits an open block into an allocated block, and then appends a free block with the remainder
+// of the space at the end
+void* split_block(void* block)
+
+// Usage: Primarily for usage of traversing a flattened linked list. Accepts a ptr to the current block and returns
+// A ptr to the next adjacent block, or NULL if it exceeds the block's size.
+void* get_next_block(void* ptr);
+
+// Usage: This function serves as the primary signature to replace the system-based malloc
+// Accepts a block size and returns an address pointing to the start of the newly allocated block
+void* mymalloc(size_t size, __FILE__, __LINE__);
+
+// Usage: Accepts a pointer to the start of some allocated portion in the block and marks that portion as
+// no longer in use. Should then run coalesce to combine blocks with all adjacent unused neighbors.
+void* myfree(void* ptr, __FILE__, __LINE__);
+
--- mymalloc.h
+++ mymalloc.h
@@ -8,10 +8,8 @@
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
 
-// This is the header
-// Negative denotes that the block is not in use
-// Positive denotes that the adjacent block is in use
-short size;
+// Defines the heap block
+static char* heap[4096];
 
 // Usage: This function serves as the primary signature to replace the system-based malloc
 // Accepts a block size and returns an address pointing to the start of the newly allocated block
@@ -20,24 +18,3 @@ void* mymalloc(size_t size, __FILE__, __LINE__);
 // Usage: Accepts a pointer to the start of some allocated portion in the block and marks that portion as
 // no longer in use. Should then run coalesce to combine blocks with all adjacent unused neighbors.
 void* myfree(void* ptr, __FILE__, __LINE__);
-
-// Usage: Traverses a linked list of block headers and returns a pointer to the first block that is greater than or equal
-// to the input size
-void* first_fit(size_t size);
-
-// Usage: Traverses a linked list of block headers and coalesces blocks that are both adjacent to each other, and freed.
-// Note: Coalescing will maintain lower addresses, and adjacent higher addresses will be absorbed
-void coalesce();
-
-void* get_previous_block(void* ptr);
-
-void* get_next_block(void* ptr);
-
-size_t round_size(size_t size);
-
-// Defines the heap block
-static char* heap[4096];
-
-// Splits an open block into an allocated block, and then appends a free block with the remainder
-// of the space at the end
-void* split_block(void* block)
