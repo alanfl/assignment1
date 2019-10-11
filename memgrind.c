df98a96799e2d3e54d86569c8c22a001b4c41692
Joshua Magnes <jdm362@pascal.cs.rutgers.edu>
2019-10-10 22:05:54 -0400

    Implement workloads E and F.

--- memgrind.c
+++ memgrind.c
@@ -41,10 +41,10 @@ double malloc_and_free_150(){
 
 	//find end time and return
 	gettimeofday(&end, NULL);
-	double elapsed = (end.tv_sec - start.tv_sec) + 
+	double timeElapsed = (end.tv_sec - start.tv_sec) + 
               ((end.tv_usec - start.tv_usec));
 
-	return elapsed;
+	return timeElapsed;
 }
 
 
@@ -75,10 +75,10 @@ double malloc_store_free(){
 	
 	//find end time, calculate length, return
 	gettimeofday(&end, NULL);
-	double elapsed = (end.tv_sec - start.tv_sec) + 
+	double timeElapsed = (end.tv_sec - start.tv_sec) + 
               ((end.tv_usec - start.tv_usec));
 
-	return elapsed;
+	return timeElapsed;
 
 }
 
@@ -141,16 +141,18 @@ double random_malloc_free_50(){
 		
 	//find end time, calculate length, return
 	gettimeofday(&end, NULL);
-	double elapsed = (end.tv_sec - start.tv_sec) + 
+	double timeElapsed = (end.tv_sec - start.tv_sec) + 
               ((end.tv_usec - start.tv_usec));
 
-	return elapsed;
+	return timeElapsed;
 }
 
 //D: Randomly choose between a randomly-sized malloc() or free()ing a pointer 
 //Keep track of each malloc so that all mallocs do not exceed your total memory 
 //capacity, malloc() 50 times, and free() all pointers.
 //Choose a random allocation size between 1 and 64 bytes
+//Worst possible scenario is 50 mallocs of max size which = 3300 bytes. This
+//cannot exceed the 4096 bytes available
 
 double all_random(){
 	//find start time	
@@ -209,32 +211,65 @@ double all_random(){
 
 	//find end time, calculate length, return
 	gettimeofday(&end, NULL);
-	double elapsed = (end.tv_sec - start.tv_sec) + 
+	double timeElapsed = (end.tv_sec - start.tv_sec) + 
               ((end.tv_usec - start.tv_usec));
 
-	return elapsed;
+	return timeElapsed;
 }
 
-//E: Undetermined
+//E: Testing to make sure that the free() function correctly stitches memory 
+//together. Malloc 64 bytes 50 times = (+2 for metadata) 66 x 50 = 3300 bytes, 796 
+//remaining. Free first 25 = 1650. Test to make sure this was correctly freed 
+//using a larger size malloc. 128 byte mallocs x 10 = 1300 bytes < 1650 but >796. 
+//Only correctly mallocs if the frees had stitched together all of the previous 
+//64 byte spaces. Will get errors if the frees did not restore the available 
+//memory from the first 25 mallocs to 1650 bytes
+//free remaining
+
 double filler_1(){
 	//find start time	
 	struct timeval start, end;
 	gettimeofday(&start, NULL);	
 
-	
 	//implement workload
+	char* ptrs [50];
+	int i;
+	int count = 0;
+	
+	//malloc 64 bytes 50 times, free 25 times
+	for(i = 0; i < 50; i++){
+		char* test = malloc(64);
+		ptrs[i] = test;
+	}	
+	for(i = 0; i < 25; i++){
+		free(ptrs[i]);
+	}	
+		
+	//malloc 128 bytes 10 times
+	for(i = 0; i < 10; i++){
+		char* test = malloc(128);
+		ptrs[i] = test;
+	}	
 
-
+	//free all pointers
+	for(i = 0; i < 50; i++){
+		if(i == 10){
+			i = 24;
+			continue;
+		}
+		free(ptrs[i]);
+	}	
 
 	//find end time, calculate length, return
 	gettimeofday(&end, NULL);
-	double elapsed = (end.tv_sec - start.tv_sec) + 
+	double timeElapsed = (end.tv_sec - start.tv_sec) + 
               ((end.tv_usec - start.tv_usec));
 
-	return elapsed;
+	return timeElapsed;
 }
 
-//F: Undetermined
+//F: Randomly sized mallocs until we are no longer able to malloc without 
+//going over 4096 bytes. Do maximum malloc size. Free all. Uses almost  
 double filler_2(){
 	//find start time
 	struct timeval start, end;
@@ -242,16 +277,44 @@ double filler_2(){
 
 	
 	//implement workload
+	char* ptrs [5000]; //tons of space because no defined number of mallocs
+	int i = 0;
+	int size;
+	int done = 0;
+	int count = 0;
+	int total = 0;
+	//run until we cannot malloc anymore
+	while(total < 4094){
+		if(total >= 4092){ //not enough space for block division
+			break;
+		}
+		size = rand() % 64 + 1; //random size 1-64
+		if((size + total) > 4094){ //4096 - 2 for first metadata
+			size = 4094 - total;
+			done = 1;
+		}
+		char* test = malloc(size);
+		ptrs[i] = test;
+		total += size + 2;
+		count++;
+		i++;
+	}
 
+	//takes between 100-130 mallocs to fill up all the space
+	//printf("There were %d mallocs\n", count);
 
+	//free all
+	for(i = 0; i < count; i++){
+		free(ptrs[i]);
+	}
 
 
 	//find end time, calculate length, return
 	gettimeofday(&end, NULL);
-	double elapsed = (end.tv_sec - start.tv_sec) + 
+	double timeElapsed = (end.tv_sec - start.tv_sec) + 
               ((end.tv_usec - start.tv_usec));
 
-	return elapsed;
+	return timeElapsed;
 }
 
 
@@ -271,7 +334,7 @@ int main (int argc, char* argv[]){
 	int i = 0;
 	//sum was occassionaly overflowing on long runtimes
 	long long sums [] = {0.0,0.0,0.0,0.0,0.0,0.0}; 
-	double averages [6];
+	double averages [] = {0.0,0.0,0.0,0.0,0.0,0.0};
 	for(i = 0; i < 100; i++){
 		sums[0] += malloc_and_free_150();
 		sums[1] += malloc_store_free();
